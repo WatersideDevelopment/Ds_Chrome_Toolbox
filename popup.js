5faf2e125bd8db90635ea42d9dedc66cdd6d2ce9
Darran Rimron <darran.rimron@beatsystems.com>
2015-08-13 17:18:57 +0100

    Trying for more graceful errors... (but getting weird errors of my own)
    -Dx

--- popup.js
+++ popup.js
@@ -6,11 +6,21 @@
 function click(e) {
   chrome.tabs.executeScript(null,
     {
-      code:"indexedDB.webkitGetDatabaseNames().onsuccess = function(sender, args) { \
+      code:"indexedDB.webkitGetDatabaseNames().onsuccess = function(sender, args) \
+      { \
 	var dbs = sender.target; \
-	console.log(sender); \
 	for(var i=0; i<dbs.result.length;i++) { \
-          indexedDB.deleteDatabase(dbs.result[i]) \
+          var databaseName = dbs.result[i]; \
+          var req = indexedDB.deleteDatabase(databaseName); \
+          req.onsuccess = function () { \
+             console.log('Deleted database ' + databaseName + ' successfully'); \
+          }; \
+          req.onerror = function () { \
+             console.error('Couldn\'t delete database ' + databaseName); \
+          }; \
+          req.onblocked = function () { \
+             console.log('Couldn\'t delete database ' + databaseName + ' due to the operation being blocked'); \
+          }; \
 	} \
       };"
     }
