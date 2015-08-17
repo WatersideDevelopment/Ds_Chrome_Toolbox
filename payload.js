3abfd721c7f6ea2d32ac00284717d7a6b6aa4e01
Darran Rimron <darran.rimron@beatsystems.com>
2015-08-17 13:47:30 +0100

    Refactored into closure...
    -Dx

--- payload.js
+++ payload.js
@@ -6,21 +6,24 @@ chrome.runtime.onMessage.addListener(function(message, sender) {
     console.log('listened');
     console.log(message);
     console.log(sender);
+
+    var deleteDatabase=function(databaseName) {
+        var req = indexedDB.deleteDatabase(databaseName);
+        req.onsuccess = function () {
+            console.log('Deleted database ' + databaseName + ' successfully');
+        };
+        req.onerror = function () {
+            console.error('Couldn\'t delete database ' + databaseName);
+        };
+        req.onblocked = function () {
+            console.log('Couldn\'t delete database ' + databaseName + ' due to the operation being blocked');
+        };
+    }
     indexedDB.webkitGetDatabaseNames().onsuccess = function(sender, args)
     {
         var dbs = sender.target;
         for(var i=0; i<dbs.result.length;i++) {
-            var databaseName = dbs.result[i];
-            var req = indexedDB.deleteDatabase(databaseName);
-            req.onsuccess = function () {
-                console.log('Deleted database ' + databaseName + ' successfully');
-            };
-            req.onerror = function () {
-                console.error('Couldn\'t delete database ' + databaseName);
-            };
-            req.onblocked = function () {
-                console.log('Couldn\'t delete database ' + databaseName + ' due to the operation being blocked');
-            };
+            indexedDB.deleteDatabase(dbs.result[i]);
         }
     };
 });
